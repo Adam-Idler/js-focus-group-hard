af225786399d08f331e7d5b6728d4b7c5ffa4131
Pavel Rakov <pasha_rakov@bk.ru>
2021-09-12 16:34:50 +0700

    Выполний 3 урок усложненное

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-const num = 266219;
-const numDigitArray = num.toString().split('');
+// 1 Задание
+const lang = prompt('Выберите локализацию: ru или en', 'ru');
+const dayOfWeek = [];
 
-const result = numDigitArray.reduce((previousValue, currentValue) => {
-    return previousValue * currentValue;
-});
-const result2= result ** 3;
+dayOfWeek['ru'] = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+dayOfWeek['en'] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-console.log(`Результат перемножения цифр числа ${num}:`, result);
-console.log('Полученный результат возведенный в 3 степень:', result2);
-console.log('Первые две цифры возведенного в 3 степень числа:', +result2.toString().substr(0, 2));
+if (lang === 'ru') {
+    console.log('Дни недели:', dayOfWeek['ru']);
+} else if (lang === 'en') {
+    console.log('Day of week:', dayOfWeek['en']);
+} else {
+    console.log('Вы выбрали неверную локализацию');
+}
+
+switch(lang) {
+    case 'ru':
+        console.log('Дни недели:', dayOfWeek['ru']);
+        break;
+    case 'en':
+        console.log('Day of week:', dayOfWeek['en']);
+        break;
+    default:
+        console.log('Вы выбрали неверную локализацию');
+}
+
+console.log(dayOfWeek[lang]);
+
+// 2 Задание
+const namePerson = prompt('Вы кто?', 'Конь в пальто');
+
+const role = namePerson === 'Артем' ? 'Директор' :
+             namePerson === 'Алекс' ? 'Преподаватель' :
+             'Студент';
+
+console.log('Ваша роль:', role);
