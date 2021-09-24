96408811a93f997f41e56e6a8ad2e3c284f90cee
Pavel Rakov <pasha_rakov@bk.ru>
2021-09-24 16:49:22 +0700

    Выполнил 4 урок усложненное

--- script.js
+++ script.js
@@ -1,36 +1,20 @@
-// 1 Задание
-const lang = prompt('Выберите локализацию: ru или en', 'ru');
-const dayOfWeek = [];
+"use strict";
 
-dayOfWeek['ru'] = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-dayOfWeek['en'] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const getString = (str) => {
+    if (typeof str !== 'string') {
+        alert('Введи строку!');
+        return;
+    }
 
-if (lang === 'ru') {
-    console.log('Дни недели:', dayOfWeek['ru']);
-} else if (lang === 'en') {
-    console.log('Day of week:', dayOfWeek['en']);
-} else {
-    console.log('Вы выбрали неверную локализацию');
+    str = str.trim();
+
+    if (str.length > 30) {
+        return str.slice(0, 30) + '...';
+    }
+
+    return str;
 }
 
-switch(lang) {
-    case 'ru':
-        console.log('Дни недели:', dayOfWeek['ru']);
-        break;
-    case 'en':
-        console.log('Day of week:', dayOfWeek['en']);
-        break;
-    default:
-        console.log('Вы выбрали неверную локализацию');
-}
+const string = prompt('Введи какую-нибудь строчку');
 
-console.log(dayOfWeek[lang]);
-
-// 2 Задание
-const namePerson = prompt('Вы кто?', 'Конь в пальто');
-
-const role = namePerson === 'Артем' ? 'Директор' :
-             namePerson === 'Алекс' ? 'Преподаватель' :
-             'Студент';
-
-console.log('Ваша роль:', role);
+console.log(getString(string));
