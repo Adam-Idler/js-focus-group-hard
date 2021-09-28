ff18ead597504237f20f7529e7df905c97d40b93
Pavel Rakov <pasha_rakov@bk.ru>
2021-09-29 00:08:40 +0700

    Выполнил вторую часть усложненного

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-"use strict";
 
-const getString = (str) => {
-    if (typeof str !== 'string') {
-        alert('Введи строку!');
-        return;
-    }
+// Задание 1
+let arr = ['1234', '4356', '34', '56754', '2987', '25', '1849'];
 
-    str = str.trim();
-
-    if (str.length > 30) {
-        return str.slice(0, 30) + '...';
-    }
-
-    return str;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i][0] === '2' || arr[i][0] === '4') {
+    console.log(arr[i]);
+  }
 }
 
-const string = prompt('Введи какую-нибудь строчку');
-
-console.log(getString(string));
+// Задание 2
+for (let i = 1; i <= 100; i++) {
+  let flag = true;
+  for (let j = 2; j < i; j++) {
+    if (i % j === 0) {
+      flag = false;
+      break;
+    }
+  }
+  if (flag) {
+    console.log(i, '- простое число, делители этого числа: 1 и', i);
+  }
+}
