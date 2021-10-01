a358316033bfc072a7f3d9d4ba74eec40cfdad1e
Pavel Rakov <pasha_rakov@bk.ru>
2021-10-01 20:40:24 +0700

    Выполнил усложненное 9 урока

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
 'use strict';
 
 let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-let date = new Date();
-let now = date.getDay() - 1;
+let months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+let hours = ['час', 'часа', 'часов'],
+    minuts = ['минута', 'минуты', 'минут'],
+    seconds = ['секунда', 'секунды', 'секунд'];
 
-week.forEach(function(item, i, arr) {
-  if (i === now) {
-    document.write(`<p><b>${item}</b></p>`);
-  } else if (i === 5 || i === 6) {
-    document.write(`<p><i>${item}</i></p>`);
+let wordForm = function(num) {
+  let n = num % 10;
+  if (num >= 5 && num <= 20) {
+    return 2; 
+  } else if (n === 1) {
+    return 0;
+  } else if (n >= 2 && n <= 4) {
+    return 1;
   } else {
-    document.write(`<p>${item}</p>`);
+    return 2;
   }
-});
+}; // 0 - (1)час, 1 - (2-4)часа, 2 - (5...)часов
+
+let addZero = function(num) {
+  if (num < 10) {
+    num = '0' + num;
+  }
+  return num;
+};
+
+let printDate = function() {
+  document.body.innerHTML = '';
+
+  let date = new Date();
+
+  let dayOfWeek = date.getDay(),
+      day = date.getDate(),
+      month = date.getMonth(),
+      year = date.getFullYear(),
+      hour = date.getHours(),
+      minute = date.getMinutes(),
+      second = date.getSeconds();
+
+  let firstString = `Сегодня ${week[dayOfWeek]}, ${day} ${months[month]} ${year} года, ${hour} ${hours[wordForm(hour)]} ${minute} ${minuts[wordForm(minute)]} ${second} ${seconds[wordForm(second)]}`;
+
+  let secondString = `${addZero(day)}.${addZero(month)}.${year} - ${addZero(hour)}:${addZero(minute)}:${addZero(second)}`;
+
+  document.write(`<p style='color:red; font-family:sans-serif; font-weight: bold'>${firstString}</p>`);
+  document.write(`<p style='color:red; font-family:sans-serif; font-weight: bold'>${secondString}</p>`);
+};
+
+let timerId = setInterval(printDate, 1000);
+printDate();
